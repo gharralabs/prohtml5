a6e7e4feb2ffdb4a301c58daa6ed5e1906c7bdaa
Marine King <mkp@prime.co.kr>
2015-09-23 13:56:41 -0300

    ch01 - fillRect

---
+++ web/web/ch01/01.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="..\Scripts/jquery-2.1.0-vsdoc.js" />
+
+$().ready(function () {
+
+    var canvas = $("#testcanvas")[0];
+    var context = canvas.getContext('2d');
+    context.fillRect(200, 10, 100, 100);    
+});
