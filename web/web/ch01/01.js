f60f2293000e0999d4fb40f591c7b7d83ec3814e
mkp <mkp>
2015-09-23 18:58:40 -0300

    ch01 - update e draw

--- web/web/ch01/01.js
+++ web/web/ch01/01.js
@@ -3,15 +3,28 @@ Math.toRadians = function (angle) {
     return angle * Math.PI / 180;
 }
 
+var nave = {
+    x: 100,
+    y: 100
+};
+
+
+var game = {};
+
 $().ready(function () {
     var canvas = $("#testcanvas")[0];
     var context = canvas.getContext('2d');
-    
+
+    game.context = context;
+
+   
     var image = new Image();
     image.src = 'images/spaceship.png';
 
+    nave.spriteSheet = image;
+
     image.onload = function () {
-        alert('Imagem foi carregada com sucesso');
+        //alert('Imagem foi carregada com sucesso');
     }
 
 
@@ -28,19 +41,26 @@ $().ready(function () {
     if (musica.canPlayType("audio/mp3") != "")
     {
         musica.oncanplaythrough = function () {
-            alert('Musica foi carregada.');
+            //alert('Musica foi carregada.');
             musica.play();
         };
-
+            
         musica.src = 'musics/03a01.mp3';
     }
 
-    
-        
-    
-
 
     
 
+    setInterval(function () {
+        nave.x += 10;
+    }, 100);
+
+    window.requestAnimationFrame(desenhar) 
 
 });
+
+function desenhar() {
+    game.context.clearRect(0, 0, 640, 480);
+    game.context.drawImage(nave.spriteSheet, 128, 0, 60, 50, nave.x, nave.y, 120, 100);
+    window.requestAnimationFrame(desenhar);
+}
