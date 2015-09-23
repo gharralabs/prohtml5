49ef2b676c7117d01be4a2287b39eaa25564e6eb
Marine King <mkp@prime.co.kr>
2015-09-23 15:28:49 -0300

    ch01 - translate, rotate, save e restore.

--- web/web/ch01/01.js
+++ web/web/ch01/01.js
@@ -1,15 +1,20 @@
 ﻿/// <reference path="..\Scripts/jquery-2.1.0-vsdoc.js" />
+Math.toRadians = function (angle) {
+    return angle * Math.PI / 180;
+}
 
 $().ready(function () {
     var canvas = $("#testcanvas")[0];
     var context = canvas.getContext('2d');
     
     var image = $("#spaceship")[0];
-    // (image, x, y)
-    context.drawImage(image, 0, 350);
-    // (image, x, y, width, height
-    context.drawImage(image, 0, 400, 100, 25);
+
+    context.save();
+    context.translate(150, 150);
+    context.rotate(Math.toRadians(180)); 
     // (image, sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height)
-    context.drawImage(image, 0, 0, 60, 50, 0, 420, 60, 50);
+    context.drawImage(image, 0, 0, 60, 50, 0, 0, 120, 100);
+    context.restore();
+
 
 });
