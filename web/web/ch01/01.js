d731f14834694a8a6bed6a109a6212a71e44910c
Marine King <mkp@prime.co.kr>
2015-09-23 15:09:27 -0300

    ch01 - desenhando imagens

--- web/web/ch01/01.js
+++ web/web/ch01/01.js
@@ -3,33 +3,13 @@
 $().ready(function () {
     var canvas = $("#testcanvas")[0];
     var context = canvas.getContext('2d');
-    context.fillStyle = "#FF0000";
-    context.fillRect(200, 10, 100, 100);
     
-    context.strokeStyle = "#0000FF";
-    context.strokeRect(110, 10, 50, 50);
-    context.clearRect(210, 20, 30, 20);
-
-    context.beginPath();
-    context.arc(100, 300, 40, Math.PI, false);
-    context.closePath();
-    context.fill();
-
-    context.beginPath();
-    context.moveTo(170, 160);
-    context.lineTo(170, 220);
-    context.lineTo(300, 220);
-    context.closePath(); // remova essa linha
-    context.stroke();
-    
-    context.fillStyle = "#000000";
-    context.font = "20 pt Arial";
-    context.fillText("ola mundo", 330, 80);
-    context.stroke();
-
-    context.fillStyle = "rgba(0, 255, 0, 0.6)";
-    context.fillRect(240, 40, 100, 100);
-
-
+    var image = $("#spaceship")[0];
+    // (image, x, y)
+    context.drawImage(image, 0, 350);
+    // (image, x, y, width, height
+    context.drawImage(image, 0, 400, 100, 25);
+    // (image, sourceX, sourceY, sourceWidth, sourceHeight, x, y, width, height)
+    context.drawImage(image, 0, 0, 60, 50, 0, 420, 60, 50);
 
 });
