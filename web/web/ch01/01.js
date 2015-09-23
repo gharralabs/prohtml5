2d9ec34e26a66d51b71b71a6231ea1b443fbb07b
Marine King <mkp@prime.co.kr>
2015-09-23 14:06:01 -0300

    CH01 - Caixas preenchidas e não preenchidas coloridas

--- web/web/ch01/01.js
+++ web/web/ch01/01.js
@@ -1,8 +1,12 @@
 ﻿/// <reference path="..\Scripts/jquery-2.1.0-vsdoc.js" />
 
 $().ready(function () {
-
     var canvas = $("#testcanvas")[0];
     var context = canvas.getContext('2d');
-    context.fillRect(200, 10, 100, 100);    
+    context.fillStyle = "#FF0000";
+    context.fillRect(200, 10, 100, 100);
+    
+    context.strokeStyle = "#0000FF";
+    context.strokeRect(110, 10, 50, 50);
+    context.clearRect(210, 20, 30, 20);
 });
