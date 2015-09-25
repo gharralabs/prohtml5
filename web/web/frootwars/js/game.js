f8bb52c4ecfcaa7dd4acfb1e821445de28896827
mkp <mkp>
2015-09-25 20:16:19 -0300

    froots war - loader

--- web/web/frootwars/js/game.js
+++ web/web/frootwars/js/game.js
@@ -1,9 +1,71 @@
 ﻿/// <reference path="../../Scripts/jquery-2.1.0-vsdoc.js" />
+var loader = {
+    loaded: true,
+    loadedCount: 0,
+    totalCount: 0,
+    soundFileExtn : ".ogg",
 
+    init: function () {
+        var mp3Support, oggSupport;
+        var audio = document.createElement('audio');
+
+        if( audio.canPlayType )
+        {
+            mp3Support = "" != audio.canPlayType("audio/mpeg");
+            oggSupport = "" != audio.canPlayType('audio/ogg; codecs="vorbis"');
+        }
+        else {
+            mp3Support = false;
+            oggSupport = false;
+        }
+
+        loader.soundFileExtn = undefined;
+
+        if (oggSupport)
+            loader.soundFileExtn = ".ogg";
+        else if (mp3Support)
+            loader.soundFileExtn = ".mp3"
+    },
+    loadImage: function (url) {
+        this.totalCount++;
+        this.loaded = false;
+        $("#loadingscreen").show();
+        var image = new Image();
+        image.src = url;
+        image.onload = loader.itemLoaded;
+        return image;
+    },
+
+    loadSound: function (url) {
+        this.totalCount++;
+        this.loaded = false;
+        $("#loadingscreen").show();
+        var audio = new Audio();
+        audio.src = url + loader.soundFileExtn;
+        audio.oncanplaythrough = loader.itemLoaded;
+        return audio;
+    },
+
+    itemLoaded: function () {
+        loader.loadedCount++;
+        $("#loadingmessage").html('Loaded ' + loader.loadedCount +
+            ' of ' + loader.totalCount);
+
+        if (loader.loadedCount === loader.totalCount) {
+            loader.loaded = true;
+            $("#loadingscreen").hide();
+
+            if (loader.onload) {
+                loader.onload();
+                loader.onload = undefined;
+            }
+        }
+    }
+}
 
 var game = {
     init: function () {
-
+        loader.init();
         levels.init();
         $(".gamelayer").hide();
         $("#gamestartscreen").show();
