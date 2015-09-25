5753c1f4ae91956d55b9edffb1a63ad60be5d331
Marine King <mkp@prime.co.kr>
2015-09-25 00:56:49 -0300

    Froots War - play e settings

--- web/web/frootwars/js/game.js
+++ web/web/frootwars/js/game.js
@@ -1,1 +1,16 @@
-﻿
+﻿/// <reference path="../../Scripts/jquery-2.1.0-vsdoc.js" />
+
+
+var game = {
+    init: function () {
+        $(".gamelayer").hide();
+        $("#gamestartscreen").show();
+
+        game.canvas = $("#gamecanvas")[0];
+        game.context = game.canvas.getContext("2d");
+    }
+};
+
+$().ready(function(){
+    game.init();
+});
